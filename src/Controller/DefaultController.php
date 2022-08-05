bfa0d5ba574b48d7c83b5340ffe8bbd6e51e3a7c
forticas <houssem@forticas.com>
2022-08-05 21:02:06 +0200

    adding static pages

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,8 +12,43 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        return $this->render('default/index.html.twig');
     }
+
+    // create a privacy policy page
+    #[Route('/privacy-policy', name: 'app_privacy_policy')]
+    public function privacyPolicy(): Response
+    {
+        return $this->render('default/privacy-policy.html.twig');
+    }
+
+    // create a terms of service page
+    #[Route('/terms-of-service', name: 'app_terms_of_service')]
+    public function termsOfService(): Response
+    {
+        return $this->render('default/terms-of-service.html.twig');
+    }
+
+    // create a contact page
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('default/contact.html.twig');
+    }
+
+    // create a about page
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('default/about.html.twig');
+    }
+
+    // create a faq page
+    #[Route('/faq', name: 'app_faq')]
+    public function faq(): Response
+    {
+        return $this->render('default/faq.html.twig');
+    }
+
+
 }
